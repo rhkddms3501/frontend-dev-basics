37265b43c17c38af970f56e988a641095593fcb4
rhkddms3501 <rhkddms3501@naver.com>
2023-02-23 15:57:12 +0900

    JavaScript & DOM

---
+++ javascript-practice/basics/ex14.js
@@ -0,0 +1,38 @@
+/**
+ * URL 다루기
+ */
+
+var url = "http://www.mysite.com/user?name=둘리&email=dooly@gmail.com";
+
+// 1. escape: URL전부를 encoding, 사용(x), deprecated
+var url2 = escape(url);
+console.log(url2);
+
+// 2. encodeURI: URL전체 중 파라미터 encoding 한다. url 전체를 encoding 해야 하는 경우 사용(O)
+var url3 = encodeURI(url);
+console.log(url3);
+
+// 3. encodeURIComponent: 값만 Encoding 해야 하는 경우, url 전체를 encoding 해야 하는 경우 사용(x), 값만 사용(O)
+var url4 = encodeURIComponent(url);
+console.log(url4);
+
+
+// 4
+// 만들어야 할 URL
+// "http://www.mysite.com/user?name=둘리&email=dooly@gmail.com";
+var url = "http://www.mysite.com/user";
+var formData = {
+    name: "둘리",
+    email: "dooly@gmail.com"
+}
+
+var toQueryString = function(o) {
+    var qs = [];
+    for(prop in o) {
+        qs.push(prop + "=" + encodeURIComponent(o[prop]))
+    }
+
+    return qs.join("&");
+}
+
+console.log(url + "?" + toQueryString(formData));
