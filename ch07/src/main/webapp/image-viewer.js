8f5a4a90db3184c970f506835d9c1d42b46a5390
rhkddms3501 <rhkddms3501@naver.com>
2023-02-28 20:58:37 +0900

    ch07/ex34.html #done

--- ch07/src/main/webapp/image-viewer.js
+++ ch07/src/main/webapp/image-viewer.js
@@ -3,34 +3,20 @@ imageViewer = {
 		$(this._init);
 	},
 	_init: function() {
-		$(".image-viewer .buttons #btn-change").click(imageViewer._change);
+		$(".image-viewer .buttons #btn-change").click(imageViewer._changeImage);
 		$(".image-viewer .buttons #btn-slideshow").click(imageViewer._slide);
 	},
-	_change: function() {
-		imageViewer._changeImage();
-	},
-	_slideClick: function() {
-		// 여기에 슬라이드 멈추는 토글 작업
-		console.log("확인확인");
-		this._slideClick = !this._slideClick;
-		imageViewer._slide;
-	},
-	_slide: function() {
-		// 여기에 슬라이드 멈추는 토글 if else 작업
-		setInterval(function() {
-			console.log("인터벌");
-			imageViewer._slideImage();
-		}, 500);
-	},
+	_index: 0,
+	_isSlideClick: false,
 	_changeImage: function() {
-		var images = this._images;
+		var images = imageViewer._images;
 		var currentImg = document.querySelector("#img").src;
 		var splitImg = currentImg.split("/");
 		var imgLingth = splitImg.length
 		var imageTemp = splitImg[imgLingth - 2] + "/" + splitImg[imgLingth - 1];
 
 		while (true) {
-			var index = Math.floor(Math.random() * this._images.length);
+			var index = Math.floor(Math.random() * imageViewer._images.length);
 			if (imageTemp != images[index].file) {
 				break;
 			}
@@ -41,32 +27,29 @@ imageViewer = {
 				title: images[index].name
 			});
 	},
-	_index: 0,
-	_slideClick: false,
+	_slide: function() {
+		imageViewer._isSlideClick = !imageViewer._isSlideClick;
+		console.warn(imageViewer._isSlideClick);
+		if (imageViewer._isSlideClick) {
+			interval = setInterval(function() {
+				imageViewer._slideImage();
+			}, 500);
+		} else {
+			clearInterval(interval);
+		}
+	},
 	_slideImage: function() {
 		var images = this._images;
 		var imagesLingth = images.length;
-			if (this._index == imagesLingth) {
-				this._index = 0;
-			}
-			$("img")
-				.attr({
-					src: images[this._index].file,
-					title: images[this._index].name
-				});
-			this._index++;
-		
-			var currentImg = document.querySelector("#img").src;
-			var currentTitle = document.querySelector("#img").title;
-			var splitImg = currentImg.split("/");
-			var imgLingth = splitImg.length
-			var imageTemp = splitImg[imgLingth - 2] + "/" + splitImg[imgLingth - 1];
-			
-			$("img")
+		if (this._index == imagesLingth) {
+			this._index = 0;
+		}
+		$("img")
 			.attr({
-				src: imageTemp,
-				title: currentTitle
+				src: images[this._index].file,
+				title: images[this._index].name
 			});
+		this._index++;
 	},
 	_images: [{
 		name: '국화',
